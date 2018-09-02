5665cadde3237c38925532fa4148478c7f1575d8
Markus Baier <mjb@MJB.fritz.box>
2018-09-02 22:29:25 +0200

    Lot of changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: 'MJB Playground',
+    description: 'Complete course about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and ressource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course,arkit 2, after effects, create sketch plugin'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'l38kxxlik5ek',
+        accessToken: '8e413a3fb16b47b60a6adcbf934702be24cdcda46212e57bfa17856632f4b289'
+      }
+    }
+
+  ],
 }
